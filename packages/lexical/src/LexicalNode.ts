e0bbe7a63de7931a580d7eb2e76170a60f64fb8b
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-07-23 12:08:15 +0200

    Start working on LexicalNode

--- packages/lexical/src/LexicalNode.ts
+++ packages/lexical/src/LexicalNode.ts
@@ -1,5 +1,55 @@
+import invariant from 'shared/invariant';
+
 export type NodeMap = Map<NodeKey, LexicalNode>;
 
 export type NodeKey = string;
 
-export class LexicalNode {}
+export class LexicalNode {
+  /** @internal */
+  __type: string;
+  /** @internal */
+  //@ts-ignore We set the key in the constructor.
+  __key: string;
+  /** @internal */
+  __parent: null | NodeKey;
+  /** @internal */
+  __prev: null | NodeKey;
+  /** @internal */
+  __next: null | NodeKey;
+
+  // Flow doesn't support abstract classes unfortunately, so we can't _force_
+  // subclasses of Node to implement statics. All subclasses of Node should have
+  // a static getType and clone method though. We define getType and clone here so we can call it
+  // on any  Node, and we throw this error by default since the subclass should provide
+  // their own implementation.
+  /**
+   * Returns the string type of this node. Every node must
+   * implement this and it MUST BE UNIQUE amongst nodes registered
+   * on the editor.
+   *
+   */
+
+  static getType(): string {
+    invariant(
+      false,
+      'LexicalNode: Node %s does not implement .getType().',
+      this.name
+    );
+  }
+
+  /**
+   * Clones this node, creating a new node with a different key
+   * and adding it to the EditorState (but not attaching it anywhere!). All nodes must
+   * implement this method.
+   *
+   */
+  static clone(_data: unknown): LexicalNode {
+    invariant(
+      false,
+      'LexicalNode: Node %s does not implement .clone().',
+      this.name
+    );
+  }
+
+  // TODO: continue here
+}
