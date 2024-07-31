f880dca1117fd57d335c407c22c8d90ec973e841
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-07-31 21:43:02 +0200

    Implement errorOnNodeKeyConstructorMismatch function

--- packages/lexical/src/LexicalUpdates.ts
+++ packages/lexical/src/LexicalUpdates.ts
@@ -1,7 +1,14 @@
 import type { LexicalEditor } from './LexicalEditor';
 
+import { EditorState } from './LexicalEditorState';
+
+let activeEditorState: null | EditorState = null;
 let activeEditor: null | LexicalEditor = null;
 
 export function internalGetActiveEditor(): LexicalEditor | null {
   return activeEditor;
+}
+
+export function internalGetActiveEditorState(): EditorState | null {
+  return activeEditorState;
 }
--- packages/lexical/src/LexicalUtils.ts
+++ packages/lexical/src/LexicalUtils.ts
@@ -1,12 +1,47 @@
-import type { LexicalNode, NodeKey, NodeMap } from './LexicalNode';
+import type { LexicalNode, NodeKey } from './LexicalNode';
+
+import invariant from 'shared/invariant';
+
+import { internalGetActiveEditorState } from './LexicalUpdates';
 
 export function $setNodeKey(
   node: LexicalNode,
   existingKey: NodeKey | null | undefined
 ): void {
   if (existingKey != null) {
-    // TODO: continue here
+    if (__DEV__) {
+      errorOnNodeKeyConstructorMismatch(node, existingKey);
+    }
     node.__key = existingKey;
     return;
+  }
+}
+
+function errorOnNodeKeyConstructorMismatch(
+  node: LexicalNode,
+  existingKey: NodeKey
+) {
+  const editorState = internalGetActiveEditorState();
+  if (!editorState) {
+    // tests expect to be able to do this kind of clone without an active editor state
+    return;
+  }
+  const existingNode = editorState._nodeMap.get(existingKey);
+  if (existingNode && existingNode.constructor !== node.constructor) {
+    // Lifted condition to if statement because the inverted logic is a bit confusing
+    if (node.constructor.name !== existingNode.constructor.name) {
+      invariant(
+        false,
+        'Lexical node with constructor %s attempted to re-use key from node in active editor state with constructor %s. Keys must not be re-used when the type is changed.',
+        node.constructor.name,
+        existingNode.constructor.name
+      );
+    } else {
+      invariant(
+        false,
+        'Lexical node with constructor %s attempted to re-use key from node in active editor state with different constructor with the same name (possibly due to invalid Hot Module Replacement). Keys must not be re-used when the type is changed.',
+        node.constructor.name
+      );
+    }
   }
 }
